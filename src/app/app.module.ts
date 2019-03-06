bf93e7baf183d50409cbca739e27391c17a52490
Tamyres Yukari Ono <tamyres@FORLOGIC.LOCAL>
2019-03-06 11:06:47 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
